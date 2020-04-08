c3dc92a7ac608ab7af9842e3875284441d792c27
Eric Chow <eric.zjp.chow@gmail.com>
2020-04-08 00:27:33 -0500

    Change to help, add more sell, hide empty, error handling, footer shortened

--- stalk-market-bot.py
+++ stalk-market-bot.py
@@ -31,6 +31,7 @@ else:
     TOKEN = os.getenv('leftovers_DISCORD_TOKEN')
 
 bot = commands.Bot(command_prefix='$')
+bot.remove_command('help')
 
 buy_prices = {}
 sell_morning_prices = {}
@@ -39,12 +40,12 @@ timezone = pytz.timezone('America/Chicago')
 
 # Bot commands
 @bot.command()
-async def info(ctx):
+async def help(ctx):
     embed = discord.Embed(
-        title='Command Menu',
+        title='Help Menu',
         description='I\'ll help you find the best prices for turnips!',
         color=discord.Colour.dark_blue())
-    embed.add_field(name='**$info**', value="List commands", inline=False)
+    embed.add_field(name='**$help**', value="List commands", inline=False)
     embed.add_field(
         name='**$all**', value="List all buy/sell prices", inline=False)
     embed.add_field(name='**$buy**',
@@ -52,7 +53,7 @@ async def info(ctx):
     embed.add_field(name='**$sell**',
                     value="List sell prices only", inline=False)
     embed.add_field(
-        name='**$add**', value="Add your price with \"$add buy n\" or \"$add sell n\"", inline=False)
+        name='**$add**', value="Add your price with \"$add buy n\", \"$add sell n\", \"$add sell n am\", \"$add sell n pm\"", inline=False)
     embed.add_field(
         name='**$clear**', value="Clear your prices with \"$clear\", \"$clear buy\", \"$clear sell\", \"$clear sell am\", \"$clear sell pm\"", inline=False)
     embed.set_footer(text="Feedback welcome. Contact chowkapow#4085")
@@ -61,88 +62,113 @@ async def info(ctx):
 
 @bot.command()
 async def all(ctx):
-    date = timezone.localize(datetime.now()).strftime("%B %d, %I:%M %p %Z")
-    embed = discord.Embed(
-        title='All Prices',
-        color=discord.Colour.dark_blue())
-    embed.add_field(name='\u200b', value='**Buy Prices**', inline=False)
-    for key, val in sorted(buy_prices.items(), key=lambda x: x[1]):
-        embed.add_field(name=key, value=val, inline=True)
-    embed.add_field(
-        name='\u200b', value='**Sell Morning Prices**', inline=False)
-    for key, val in sorted(sell_morning_prices.items(), key=lambda x: x[1], reverse=True):
-        embed.add_field(name=key, value=val, inline=True)
-    embed.add_field(
-        name='\u200b', value='**Sell Afternoon Prices**', inline=False)
-    for key, val in sorted(sell_afternoon_prices.items(), key=lambda x: x[1], reverse=True):
-        embed.add_field(name=key, value=val, inline=True)
-    embed.set_footer(text='Timestamp: ' + date)
-    await ctx.send(embed=embed)
+    if len(buy_prices) == 0 and len(sell_morning_prices) == 0 and len(sell_afternoon_prices) == 0:
+        await ctx.send("No prices at this time.")
+    else:
+        date = timezone.localize(datetime.now()).strftime("%B %d, %I:%M %p %Z")
+        embed = discord.Embed(
+            title='All Prices',
+            color=discord.Colour.dark_blue())
+        if len(buy_prices) > 0:
+            embed.add_field(
+                name='\u200b', value='**Buy Prices**', inline=False)
+            for key, val in sorted(buy_prices.items(), key=lambda x: x[1]):
+                embed.add_field(name=key, value=val, inline=True)
+        if len(sell_morning_prices) > 0:
+            embed.add_field(
+                name='\u200b', value='**Sell Morning Prices**', inline=False)
+            for key, val in sorted(sell_morning_prices.items(), key=lambda x: x[1], reverse=True):
+                embed.add_field(name=key, value=val, inline=True)
+        if len(sell_afternoon_prices) > 0:
+            embed.add_field(
+                name='\u200b', value='**Sell Afternoon Prices**', inline=False)
+            for key, val in sorted(sell_afternoon_prices.items(), key=lambda x: x[1], reverse=True):
+                embed.add_field(name=key, value=val, inline=True)
+        embed.set_footer(text=date)
+        await ctx.send(embed=embed)
 
 
 @bot.command()
 async def buy(ctx):
-    date = timezone.localize(datetime.now()).strftime("%B %d, %I:%M %p %Z")
-    embed = discord.Embed(
-        title='Buy Prices',
-        color=discord.Colour.dark_blue())
-    for key, val in sorted(buy_prices.items(), key=lambda x: x[1]):
-        embed.add_field(name=key, value=val, inline=True)
-    embed.set_footer(text='Timestamp: ' + date)
-    await ctx.send(embed=embed)
+    if len(buy_prices) == 0:
+        await ctx.send("No buy prices at this time.")
+    else:
+        date = timezone.localize(datetime.now()).strftime("%B %d, %I:%M %p %Z")
+        embed = discord.Embed(
+            title='Buy Prices',
+            color=discord.Colour.dark_blue())
+        for key, val in sorted(buy_prices.items(), key=lambda x: x[1]):
+            embed.add_field(name=key, value=val, inline=True)
+        embed.set_footer(text=date)
+        await ctx.send(embed=embed)
 
 
 @bot.command()
 async def sell(ctx):
-    date = timezone.localize(datetime.now()).strftime("%B %d, %I:%M %p %Z")
-    embed = discord.Embed(
-        title='Sell Prices',
-        color=discord.Colour.dark_blue())
-    embed.add_field(name='\u200b', value='Sell Morning Prices', inline=False)
-    for key, val in sorted(sell_morning_prices.items(), key=lambda x: x[1], reverse=True):
-        embed.add_field(name=key, value=val, inline=True)
-    embed.add_field(name='\u200b', value='Sell Afternoon Prices', inline=False)
-    for key, val in sorted(sell_afternoon_prices.items(), key=lambda x: x[1], reverse=True):
-        embed.add_field(name=key, value=val, inline=True)
-    embed.set_footer(text='Timestamp: ' + date)
-    await ctx.send(embed=embed)
+    if len(sell_morning_prices) == 0 and len(sell_afternoon_prices) == 0:
+        await ctx.send("No sell prices at this time.")
+    else:
+        date = timezone.localize(datetime.now()).strftime("%B %d, %I:%M %p %Z")
+        embed = discord.Embed(
+            title='Sell Prices',
+            color=discord.Colour.dark_blue())
+        if len(sell_morning_prices) > 0:
+            embed.add_field(
+                name='\u200b', value='Sell Morning Prices', inline=False)
+            for key, val in sorted(sell_morning_prices.items(), key=lambda x: x[1], reverse=True):
+                embed.add_field(name=key, value=val, inline=True)
+        if len(sell_afternoon_prices) > 0:
+            embed.add_field(
+                name='\u200b', value='Sell Afternoon Prices', inline=False)
+            for key, val in sorted(sell_afternoon_prices.items(), key=lambda x: x[1], reverse=True):
+                embed.add_field(name=key, value=val, inline=True)
+        embed.set_footer(text=date)
+        await ctx.send(embed=embed)
 
 
 @bot.command()
-async def add(ctx, op: str, price: int):
-    await ctx.send('Added {0.author.name}\'s {1} price of {2}.'.format(ctx, op, price))
-    if op == 'buy':
-        buy_prices[ctx.author.name] = price
-        await buy(ctx)
+async def add(ctx, op='sell', price='0', selltime=''):
+    selltime = selltime.lower()
+    if (op != 'buy' and op != 'sell') or not price.isdigit() or (selltime != '' and selltime != 'am' and selltime != 'pm'):
+        await ctx.send("Please check your input.")
     else:
-        if datetime.now().hour < 12:
-            sell_morning_prices[ctx.author.name] = price
+        await ctx.send('Added {0.author.name}\'s {1} price of {2}.'.format(ctx, op, price))
+        if op == 'buy':
+            buy_prices[ctx.author.name] = price
+            await buy(ctx)
         else:
-            sell_afternoon_prices[ctx.author.name] = price
-        await sell(ctx)
+            if (datetime.now().hour < 12 and selltime == '') or selltime == 'am':
+                sell_morning_prices[ctx.author.name] = int(price)
+            elif (datetime.now().hour >= 12 and selltime == '') or selltime == 'pm':
+                sell_afternoon_prices[ctx.author.name] = int(price)
+            await sell(ctx)
 
 
 @bot.command()
 async def clear(ctx, op='', selltime=''):
-    if op == 'buy':
-        buy_prices.pop(ctx.author.name, None)
-        await ctx.send("Cleared {0}'s buy price.".format(ctx.author.name))
-    elif op == 'sell':
-        if selltime.lower() == 'am':
-            sell_morning_prices.pop(ctx.author.name, None)
-            await ctx.send("Cleared {0}'s sell morning prices.".format(ctx.author.name))
-        elif selltime.lower() == 'pm':
-            sell_afternoon_prices.pop(ctx.author.name, None)
-            await ctx.send("Cleared {0}'s sell afternoon price.".format(ctx.author.name))
-        else:
-            sell_morning_prices.pop(ctx.author.name, None)
-            sell_afternoon_prices.pop(ctx.author.name, None)
-            await ctx.send("Cleared {0}'s sell prices.".format(ctx.author.name))
+    selltime = selltime.lower()
+    if (op != '' and op != 'buy' and op != 'sell') or (selltime != '' and selltime != 'am' and selltime != 'pm'):
+        await ctx.send("Please check your input.")
     else:
-        buy_prices.pop(ctx.author.name, None)
-        sell_morning_prices.pop(ctx.author.name, None)
-        sell_afternoon_prices.pop(ctx.author.name, None)
-        await ctx.send("Cleared {0}'s buy/sell prices.".format(ctx.author.name))
+        if op == 'buy':
+            buy_prices.pop(ctx.author.name, None)
+            await ctx.send("Cleared {0}'s buy price.".format(ctx.author.name))
+        elif op == 'sell':
+            if selltime == 'am':
+                sell_morning_prices.pop(ctx.author.name, None)
+                await ctx.send("Cleared {0}'s sell morning price.".format(ctx.author.name))
+            elif selltime == 'pm':
+                sell_afternoon_prices.pop(ctx.author.name, None)
+                await ctx.send("Cleared {0}'s sell afternoon price.".format(ctx.author.name))
+            else:
+                sell_morning_prices.pop(ctx.author.name, None)
+                sell_afternoon_prices.pop(ctx.author.name, None)
+                await ctx.send("Cleared {0}'s sell prices.".format(ctx.author.name))
+        else:
+            buy_prices.pop(ctx.author.name, None)
+            sell_morning_prices.pop(ctx.author.name, None)
+            sell_afternoon_prices.pop(ctx.author.name, None)
+            await ctx.send("Cleared {0}'s buy/sell prices.".format(ctx.author.name))
 
 # Background task to clear prices
 @tasks.loop(hours=168)
